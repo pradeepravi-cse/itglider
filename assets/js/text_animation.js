18dccb7b20eeb0ff1dff536a7657b845842f8f6a
Pradeep <admin@Pradeeps-MacBook-Pro.local>
2022-02-07 08:38:01 +0530

    initial commit

---
+++ assets/js/text_animation.js
@@ -0,0 +1,18 @@
+function tm_animate_text() {
+  "use strict";
+
+  var animateSpan = jQuery(".animation_text_word");
+
+  animateSpan.typed({
+    strings: ["ITGlider", "ITGlider", "ITGlider"],
+    loop: true,
+    startDelay: 1e3,
+    backDelay: 3e3,
+  });
+}
+
+jQuery(document).on("ready", function () {
+  (function ($) {
+    tm_animate_text();
+  })(jQuery);
+});
